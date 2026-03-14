f4b9ba8ada023dab8af6bd53d836eeb819a9098e
Kshitij <kshitijguladhe2002@gmail.com>
2026-03-14 13:03:38 +0530

    temporary rename

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import HeroSection from "../components/HeroSection";
+import HeroSection from "../components/HeroSectionTemp";
 import DestCard from "../components/DestCard";
 import BlogCard from "../components/BlogCard";
 import Footer from "../components/Footer";
